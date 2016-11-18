8053e8058938a570e52023fd893e42dd6af63ec5
Josh Symonds <veraticus@gmail.com>
2016-11-17 19:45:12 -0600

    Update sysctl version

--- metadata.rb
+++ metadata.rb
@@ -21,7 +21,7 @@ maintainer_email 'dominik.richter@googlemail.com'
 license          'Apache 2.0'
 description      'Installs and configures operating system hardening'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.0.1'
 
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 6.0'
@@ -29,7 +29,7 @@ supports 'centos', '>= 5.0'
 supports 'redhat', '>= 5.0'
 supports 'oracle', '>= 6.4'
 
-depends 'sysctl', '<= 0.7.5'
+depends 'sysctl', '~> 0.8'
 depends 'apt', '~> 3.0.0'
 depends 'yum'
 
